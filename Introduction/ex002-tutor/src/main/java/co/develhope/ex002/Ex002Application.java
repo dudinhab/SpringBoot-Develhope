2b3fd6d68233b4a7358d4cb4514373f7899f5e33
Maria Eduarda Barone <duda.barone@outlook.com>
2024-08-16 21:08:34 +0200

    ex002

---
+++ Introduction/ex002-tutor/src/main/java/co/develhope/ex002/Ex002Application.java
@@ -0,0 +1,13 @@
+package co.develhope.ex002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex002Application.class, args);
+	}
+
+}
---
+++ Introduction/ex002-tutor/src/test/java/co/develhope/ex002/Ex002ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.ex002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
